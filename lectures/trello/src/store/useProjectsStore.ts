4c10c091c338474925fe0a3926a642172fac16b5
Paul Landaeta <paulwilkerlf@gmail.com>
2025-06-11 16:47:09 -0400

    fix: add usehook and zustand

--- lectures/trello/src/store/useProjectsStore.ts
+++ lectures/trello/src/store/useProjectsStore.ts
@@ -1,6 +1,11 @@
 import { create } from "zustand";
 import type { Project } from "../interfaces/projectInterface";
-import { deleteProject, getProjectByUserId } from "../services/projectService";
+import {
+  createProject,
+  deleteProject,
+  getProjectByUserId,
+  updateProject,
+} from "../services/projectService";
 
 interface ProjectStore {
   projects: Project[];
@@ -9,6 +14,8 @@ interface ProjectStore {
 
   fetchProjects: (userId: string) => void;
   removeProject: (projectId: string) => void;
+  createProject: (project: Project) => void;
+  updateProject: (project: Project) => void;
 }
 
 export const useProjectsStore = create<ProjectStore>((set) => ({
@@ -37,6 +44,47 @@ export const useProjectsStore = create<ProjectStore>((set) => ({
       await deleteProject(projectId);
       set((state) => ({
         projects: state.projects.filter((project) => project.id !== projectId),
+      }));
+    } catch (error) {
+      console.log(error);
+      if (error instanceof Error) {
+        set({ error: error.message });
+      }
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+
+  createProject: async (project: Project) => {
+    try {
+      set({ isLoading: true });
+      const projectResponse = await createProject(project);
+      if (!projectResponse) {
+        throw new Error("No se logro Crear el proyecto");
+      }
+      set((state) => ({
+        projects: [...state.projects, projectResponse],
+      }));
+    } catch (error) {
+      console.log(error);
+      if (error instanceof Error) {
+        set({ error: error.message });
+      }
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+  updateProject: async (project: Project) => {
+    try {
+      set({ isLoading: true });
+      const projectResponse = await updateProject(project);
+      if (!projectResponse) {
+        throw new Error("No se logro Actualizar el proyecto");
+      }
+      set((state) => ({
+        projects: state.projects.map((project) =>
+          project.id === projectResponse.id ? projectResponse : project
+        ),
       }));
     } catch (error) {
       console.log(error);
