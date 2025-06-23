a4ed0086d99cf790ea4757dcb03231426d43eb58
Paul Landaeta <paulwilkerlf@gmail.com>
2025-06-23 18:43:48 -0400

    feat: message

--- lectures/sse/index.js
+++ lectures/sse/index.js
@@ -1,31 +1,40 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const PORT = 5010;
 
 app.use(cors());
+app.use(express.json());
 
-app.get('/events', (req, res) => {
-    res.setHeader('Content-Type', 'text/event-stream');
-    res.setHeader('Cache-Control', 'no-cache');
-    res.setHeader('Connection','keep-alive');
+let clientes = [];
+app.get("/events", (req, res) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
 
-    res.flushHeaders();
+  res.flushHeaders();
 
-    const sendMessage = () => {
-        const data = { date: new Date().toISOString() };
-        res.write(`data: ${JSON.stringify(data)}\n\n`);
-    };
+  clientes.push(res);
 
-    const intervalId = setInterval(sendMessage, 2000);
-
-    res.on('close', () => {
-        clearInterval(intervalId);
-        res.end();
-    })
+  res.on("close", () => {
+    clientes = clientes.filter((client) => {
+      client !== res;
+    });
+  });
 });
 
-app.listen( PORT, ()=>{
-    console.log(`El servidor esta UP en el puerto: ${PORT}`);
-})
+app.post("/sendMessage", (req, res) => {
+  const { message } = req.body;
+  clientes.forEach((client) => {
+    client.write(`data: ${JSON.stringify(message)}\n\n`);
+  });
+  res.status(200).json({
+    sent: true,
+    message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`El servidor esta UP en el puerto: ${PORT}`);
+});
